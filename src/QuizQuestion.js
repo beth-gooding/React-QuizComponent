6b98da6e7658ee48154b0e0e46e1b3eea030fab0
Beth Gooding <beth.gooding@hotmail.com>
2021-10-18 16:31:07 +0100

    Attempts step 7: Displaying an error message based on state

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,14 +2,22 @@ import React, {Component} from 'react';
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {incorrectAnswer: false};
+    }
     handleClick(buttonText){
         if (buttonText === this.props.quiz_question.answer) {
+            this.setState({...this.state, incorrectAnswer: false})
             this.props.showNextQuestionHandler();
+        } else {
+            this.setState({...this.state, incorrectAnswer: true})
         }
     }
     render() {
         return (
             <main>
+                {this.state.incorrectAnswer && <p className="error">Sorry, that's not right</p>}
             <section>
               <p>{this.props.quiz_question.instruction_text}</p>
             </section>
